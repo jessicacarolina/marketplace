2f445ba0cedd554239e2159486ccb469436f80f6
Jéssica <jessica.silva@mazzafc.tech>
2020-08-05 09:40:46 -0300

    Adding the cart

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index()
+    {
+        $cart = session()->has('cart') ? session()->get('cart') : [];
+
+        return view('cart', compact('cart'));
+    }
+
+    public function add(Request $request)
+    {
+        $product = $request->get('product');
+
+       // verificar se existe sessao para o produto
+        if (session()->has('cart')) {
+            // Existindo eu adiciono este produto na sessao existente
+            // Antes de adicionar, verifico se o produto já existe no carrinho e só incremento a qtde;
+
+            $products = session()->get('cart');
+            $productsSlugs = array_column($products, 'slug');
+
+            if (in_array($product['slug'], $productsSlugs)) {
+                $products = $this->productIncrement($product['slug'], $product['amount'], $products);
+                session()->put('cart', $products);
+            } else {
+                session()->push('cart', $product);
+            }
+
+        } else{
+            // Não existindo eu crio esta sessao com o primeiro produto
+            $products[] = $product;
+
+            session()->put('cart', $products);
+        }
+
+        flash('Produto adicionado no carrinho!')->success();
+        return redirect()->route('product.single', ['slug' => $product['slug']]);
+    }
+
+    public function remove($slug)
+    {
+        if (!session()->has('cart'))
+            return redirect()->route('cart.index');
+
+        $products = session()->get('cart');
+
+        $products = array_filter($products, function ($line) use($slug) {
+           return $line['slug'] != $slug;
+        });
+
+        session()->put('cart', $products);
+        return redirect()->route('cart.index');
+    }
+
+    public function cancel()
+    {
+        session()->forget('cart');
+
+        flash('Compra cancelada!')->success();
+        return redirect()->route('cart.index');
+    }
+
+    private function productIncrement($slug, $amount, $products)
+    {
+        $products = array_map(function ($line) use ($slug, $amount) {
+            if ($slug == $line['slug']) {
+                $line['amount'] += $amount;
+            }
+            return $line;
+        }, $products);
+
+        return $products;
+    }
+}
